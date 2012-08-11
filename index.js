02c34d1383dfc752088c5cf1ac3fbe5593bd693d
Carlos Rodriguez <carlos@s8f.org>
2012-08-10 17:40:08 -0700

    new features

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var Middler = require('./lib/middler')
+  , isArray = require('util').isArray
 
 module.exports = function middler(server, fn) {
   if (!server._middler) {
     server._middler = new Middler(server);
   }
   if (fn) {
-    server._middler.add(fn);
+    if (!isArray(fn)) {
+      fn = [fn];
+    }
+    fn.forEach(function(f) {
+      server._middler.add(f);
+    });
   }
+  return server._middler;
 };
