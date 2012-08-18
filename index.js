44d1598dfe61d9dc9f960427ded85755aa02256e
Carlos Rodriguez <carlos@s8f.org>
2012-08-18 15:21:13 -0700

    refactor for embeddable api

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-var Middler = require('./lib/middler')
-  , isArray = require('util').isArray
-
-module.exports = function middler(server, fn) {
-  if (!server._middler) {
-    server._middler = new Middler(server);
-  }
-  if (fn) {
-    if (!isArray(fn)) {
-      fn = [fn];
-    }
-    fn.forEach(function(f) {
-      server._middler.add(f);
-    });
-  }
-  return server._middler;
+module.exports = exports = function middler (server, fn) {
+  if (server) return exports.attach(server, fn);
+  return new exports.Middler();
 };
+
+exports.parseUrl = require('./lib/parseUrl');
+exports.pathRegExp = require('./lib/pathRegExp');
+exports.matcher = require('./lib/matcher');
+exports.Middler = require('./lib/middler');
+exports.attach = require('./lib/attach');
