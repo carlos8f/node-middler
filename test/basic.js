9633fd0e0b50d4707aea202c61029f5f7c30cadc
Carlos Rodriguez <carlos@s8f.org>
2012-08-17 22:02:51 -0700

    traditional syntax

--- test/basic.js
+++ test/basic.js
@@ -5,11 +5,10 @@ describe('basic test', function () {
   it('serves basic middleware', function (done) {
     listen(function (server, port) {
       server.once('close', done);
-      middler(server)
-        .add(function (req, res, next) {
-          res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-          res.end('hello world');
-        });
+      middler(server, function (req, res, next) {
+        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+        res.end('hello world');
+      });
 
       request.get('http://localhost:' + port + '/', function (res) {
         assert.equal(res.statusCode, 200);
