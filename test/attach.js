a46952049749438b97999d1b69333e27a0762e1c
Carlos Rodriguez <carlos@s8f.org>
2012-08-19 14:11:17 -0700

    add support for attaching an existing middler instance

---
+++ test/attach.js
@@ -0,0 +1,30 @@
+describe('attach', function () {
+  var server, port;
+  before(function (done) {
+    listen(function (s, p) {
+      server = s;
+      port = p;
+      done();
+    });
+  })
+  it('can attach existing middler instance', function (done) {
+    var instance = middler()
+      .add(function (req, res, next) {
+        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+        res.end('hello world');
+      });
+    middler(server, instance);
+
+    request.get('http://localhost:' + port + '/', function (res) {
+      assert.equal(res.statusCode, 200);
+      assert.equal(res.headers['content-type'], 'text/plain; charset=utf-8');
+      assert.equal(res.text, 'hello world');
+      done();
+    });
+  });
+
+  it('closes the server', function (done) {
+    server.once('close', done);
+    server.close();
+  });
+});
