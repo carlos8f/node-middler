60b0e3be500db36c5a1846b4a6f7e45b2fc8bded
Carlos Rodriguez <carlos@s8f.org>
2013-07-12 11:53:19 -0700

    add vhost matching support

---
+++ test/vhost.js
@@ -0,0 +1,42 @@
+describe('vhost matching', function () {
+  var baseUrl, baseUrl2, server;
+  before(function (done) {
+    listen(function (s, port) {
+      server = s;
+      baseUrl = 'http://localhost:' + port;
+      baseUrl2 = 'http://127.0.0.1:' + port;
+      middler(server)
+        .add('*', function (req, res, next) {
+          res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+          res.write('hi, ');
+          next();
+        })
+        .add('*lhost', function (req, res, next) {
+          res.end('i am localhost');
+        })
+        .add('127.0.0.1', function (req, res, next) {
+          res.end('i am ipv4');
+        });
+      done();
+    });
+  });
+
+  it('get localhost', function (done) {
+    request.get(baseUrl + '/', function (res) {
+      assertRes(res, 'hi, i am localhost');
+      done();
+    });
+  });
+
+  it('get ipv4', function (done) {
+    request.get(baseUrl2 + '/', function (res) {
+      assertRes(res, 'hi, i am ipv4');
+      done();
+    });
+  });
+
+  it('close', function (done) {
+    server.once('close', done);
+    server.close();
+  });
+});
